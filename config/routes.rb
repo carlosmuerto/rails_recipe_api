2c69f360df22b574ecc960006df98cafa0966a3d
Carlos A. Vivas N <cavivasnieto@gmail.com>
2022-12-27 00:23:34 +0000

    Install cancancan and update users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   devise_for :users
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
