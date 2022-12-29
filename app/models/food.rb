241f4863360b67c98c4e60ff26411e7eb5ff1c00
Carlos A. Vivas N <cavivasnieto@gmail.com>
2022-12-29 02:39:09 +0000

    add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,11 @@
 class Food < ApplicationRecord
+  has_many :recipe_food
+  has_many :recipes, through: :recipe_food, source: :recipe
+
+  has_many :user_food
+  has_many :users, through: :user_food, source: :user
+
+  validates :name, presence: true
+  validates :unit, presence: true, inclusion: { in: %w[g Kg lts ml piece] }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_food
+  has_many :foods, through: :recipe_food, source: :food
+
+  validates :foods, length: { minimum: 1 }
+  validates :name, presence: true
+  validates :preparation_time_seconds, presence: true, numericality: { greater_than: 0 }
+  validates :cooking_time_seconds, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true
 end
