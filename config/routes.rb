d9fa492c2f9bdee0660cf77f8e13109253e21b01
Carlos A. Vivas N <cavivasnieto@gmail.com>
2022-12-30 01:11:22 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :user_foods
-  resources :recipe_foods
-  resources :recipes
-  resources :foods
-  resources :users
+  resources :recipe_food, only: [:index, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    get 'publics', on: :collection
+  end
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
@@ -24,3 +25,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "current_user#index"
 end
+
+
+
+
+
+  
+
+  
