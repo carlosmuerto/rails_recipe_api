905b68cfe203418622e36b02f4c8caf21b0fcea0
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-02-23 22:32:18 -0400

    Update Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,20 @@
 class Ability
   include CanCan::Ability
+
+  
+
+  def initialize(user)
+    can(%i[read], Recipe, :public true)
+
+    return unless user.present?
+
+    can(%i[read], Food, user:)
+    can(%i[read create update destroy], Recipe, author: user)
+    can(%i[read create update destroy], UserFood, user:)
+    can(%i[read create update destroy], RecipeFood)
+
+    return unless user.admin?
+
+    can %i[read update create destroy], :all
+  end
 end
