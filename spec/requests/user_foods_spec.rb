5ea13e2cacbbee9eafbd5f0c282826629a1f129f
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-02-24 21:48:35 -0400

    Add User_food specs + swaggerize

--- spec/requests/user_foods_spec.rb
+++ spec/requests/user_foods_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'UserFood', type: :request do
       password: '123rspec123'
     )
   end
-  let(:valid_attributes) do
+  let(:valid_food) do
     Food.create(
       name: 'Test Food',
       unit: 'Kg',
@@ -34,6 +34,70 @@ RSpec.describe 'UserFood', type: :request do
 
         let(:Authorization) do
           Devise::JWT::TestHelpers.auth_headers({}, test_person)['Authorization']
+        end
+
+        run_test!
+      end
+    end
+
+    post 'Create a User food' do
+      consumes 'application/json'
+      produces 'application/json'
+
+      security [{ bearer_auth: [] }]
+
+      parameter name: :UserFood, in: :body, schema: {
+        type: :object,
+        properties: {
+          food_id: { type: :integer, example: 2 },
+          quantity: { type: :number, example: 0.01 },
+        },
+        required: %w[
+          food_id
+          quantity
+        ]
+      }
+
+      response 401, 'Unauthorized' do
+        let(:Authorization) { '' }
+
+        let(:UserFood) do
+          {
+            food_id: valid_food.id,
+            quantity: 1.1
+          }
+        end
+
+        run_test!
+      end
+
+      response 422, 'Unprocessable Entity' do
+        let(:Authorization) do
+          Devise::JWT::TestHelpers.auth_headers({}, test_person)['Authorization']
+        end
+
+        let(:UserFood) do
+          {
+            food_id: valid_food.id,
+            quantity: -1.1
+          }
+        end
+
+        run_test!
+      end
+
+      response 201, 'Created' do
+        schema '$ref' => '#/components/schemas/UserFood'
+
+        let(:Authorization) do
+          Devise::JWT::TestHelpers.auth_headers({}, test_person)['Authorization']
+        end
+
+        let(:UserFood) do
+          {
+            food_id: valid_food.id,
+            quantity: 1.1
+          }
         end
 
         run_test!
