f8b40693f1c2106cb50fd295e573e952f40365a8
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-03-01 15:43:35 -0400

    Fix recipe food creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,44 +1,56 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_recipe, only: %i[show destroy]
+  load_and_authorize_resource
 
   # GET /recipes
   def index
-    @recipes = Recipe.all
+    # @recipes = Food.all
 
-    render json: @recipes
+    render json: @recipes.map { |item|
+      RecipeSerializer.new(item).serializable_hash[:data][:attributes]
+    }, status: :ok
   end
 
   # GET /recipes/1
   def show
-    render json: @recipe
+    render json: RecipeSerializer.new(@recipe).serializable_hash[:data][:attributes], status: :ok
   end
 
   # POST /recipes
   def create
     @recipe = Recipe.new(recipe_params)
+    @recipe.author = current_user
+
+    @recipe.recipe_food =  params.require(:recipe)
+      .except(:name, :preparation_time, :cooking_time, :description, :public)
+      .permit(foods: [:food_id, :quantity])[:foods].map do |recipe_food|
+      RecipeFood.new(
+        food: Food.find(recipe_food.require(:food_id)),
+        quantity: recipe_food.require(:quantity)
+      )
+    end
 
     if @recipe.save
-      render json: @recipe, status: :created, location: @recipe
+      render json: RecipeSerializer.new(@recipe).serializable_hash[:data][:attributes], status: :created
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render json: {
+        item: RecipeSerializer.new(@recipe).serializable_hash[:data][:attributes],
+        errors: @recipe.errors
+      },
+      status: :unprocessable_entity
     end
   end
 
   # DELETE /recipes/1
   def destroy
     @recipe.destroy
+    render json: { message: 'recipe deleted!' }, status: :no_content
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
-  end
-
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+    params.require(:recipe).except(:foods).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,15 @@
+class RecipeSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :public, :preparation_time, :cooking_time
+  attribute :author do |recipe|
+    UserSerializer.new(recipe.author).serializable_hash[:data][:attributes]
+  end
+  attribute :foods do |recipe|
+    recipe.recipe_food.map do |rc_fd|
+      {
+        food: FoodSerializer.new(rc_fd.food).serializable_hash[:data][:attributes],
+        quantity: rc_fd.quantity
+      }
+    end
+  end
+end
